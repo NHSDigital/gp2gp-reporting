4db17ec521cad620022587ff0d61c59a71f841c9
Sam <73955537+samfallowfield@users.noreply.github.com>
2026-01-21 17:05:46 +0000

    [PRM-627-6] (#79)
    * [PRM-627-1] - update the terraform stack passed to the infrastructure yml, as they have not been updated to use "_" over "-"
    
    * [PRM-627-1] - updated python packages
    
    * [PRM-627-1] - updated format
    
    * [PRM-627-2] - image failure fix
    
    * [PRM-627-6] - update urllib in setup.py
    
    * [PRM-627-6] - update pyarrow in setup.py

--- services/reports-generator/setup.py
+++ services/reports-generator/setup.py
@@ -8,8 +8,8 @@ setup(
     install_requires=[
         "python-dateutil>=2.8",
         "boto3>=1.18",
-        "urllib3==1.26.18",
-        "PyArrow==22.0.0",
+        "urllib3==2.6.3",
+        "PyArrow==23.0.0",
         "polars~=0.20.31",
     ],
 )
